ac8a99e6e6a6b822ea5187a81babf8c4cdb09691
Shubham Naik <shub@shub.club>
2017-04-28 14:55:38 -0700

    added insert pole

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,10 @@
 #include <vector>
 #include <string>
 
+// foreach
+#include <boost/foreach.hpp>
+#define forEach BOOST_FOREACH
+
 // defaults
 const int ZONE_TABLE_MAX_X = 8;
 const int ZONE_TABLE_MAX_Y = 8;
@@ -17,7 +21,7 @@ const int ZONE_COUNT = ZONE_TABLE_MAX_X*2 * ZONE_TABLE_MAX_Y*2;
 
 // Declare handler
 
-ros::NodeHandle nh;
+
 geometry_msgs::Point currentLocation;
 std::vector<geometry_msgs::Point> zoneList(ZONE_COUNT);
 std::vector<geometry_msgs::Point> poleList;
@@ -63,7 +67,7 @@ void incrementZone(){
 }
 
 void addPole(geometry_msgs::Point poleLocation){
-  poleList.insert(poleLocation);
+  poleList.push_back(poleLocation);
   ROS_INFO_STREAM("ADDED POLE AT x:" << poleLocation.x << " y:" << poleLocation.y);
 }
 
@@ -90,50 +94,54 @@ void verifyAndInsertPole(geometry_msgs::Point poleLocation){
 
 
 
-void moveRobot(float x, float y){
+void moveRobot(ros::NodeHandle nh, float x, float y){
   // initialize subscriber base
   ros::ServiceClient client = nh.serviceClient<nav_msgs::GetPlan>("/move_base/make_plan");
   client.waitForExistence();
 
   nav_msgs::GetPlan service;
 
+  ROS_INFO("init: x = %f, y = %f", currentLocation.x, currentLocation.y);
+
   service.request.start.header.frame_id = "map";
   service.request.start.header.stamp = ros::Time::now();
   service.request.start.pose.position.x = currentLocation.x;
   service.request.start.pose.position.y = currentLocation.y;
-  //service.request.start.pose.orientation.w = tmppose.orientation.w;    
+  service.request.start.pose.orientation.w = 0;    
 
   service.request.goal.header.frame_id = "map";
   service.request.goal.header.stamp = ros::Time::now();
   service.request.goal.pose.position.x = x;
   service.request.goal.pose.position.y = y;
-  //service.request.goal.pose.orientation.w = poi.orientation.w;
+  service.request.goal.pose.orientation.w = 0;
 
   currentLocation.x = x;
   currentLocation.y = y;
 
   if(client.call(service)){
     ROS_INFO_STREAM("Service successfully called");
+
+   forEach(const geometry_msgs::PoseStamped &p, service.response.plan.poses) {
+      ROS_INFO("x = %f, y = %f", p.pose.position.x, p.pose.position.y);
+   }
+
   } else {
     ROS_ERROR_STREAM("Error while calling service");
   }
 
-  for(int i = 0; i < service.response.plan.poses.size(); i++){
-    ROS_INFO_STREAM("plan point " << i << ":" << service.response.plan.poses[i]);
-  }
+ 
 
 
-
-  if(client.getState() == actionlib::SimpleClientGoalState::SUCCEEDED){
+  /*if(client.getState() == actionlib::SimpleClientGoalState::SUCCEEDED){
 		ROS_INFO("MOVED SUCCESFULLY");
-    scanPoles();
+    //scanPoles();
     incrementZone();
     moveRobot(zoneList[zoneHandled].x, zoneList[zoneHandled].y);
 	} else {
     ROS_INFO("MOVED FAILED");
     incrementZone();
     moveRobot(zoneList[zoneHandled].x, zoneList[zoneHandled].y);
-	}
+	}*/
 
   ros::spin();
 }
@@ -148,6 +156,7 @@ void scanPoles(){
 int main(int argc,char **argv){
   ros::init(argc,argv,"main");
 
+  ros::NodeHandle nh;
   
   // initialize routes
   setOriginLocation();
@@ -155,7 +164,7 @@ int main(int argc,char **argv){
 
   
 
-  moveRobot(zoneList[zoneHandled].x, zoneList[zoneHandled].y);
+  moveRobot(nh, zoneList[zoneHandled].x, zoneList[zoneHandled].y);
 
   
 
